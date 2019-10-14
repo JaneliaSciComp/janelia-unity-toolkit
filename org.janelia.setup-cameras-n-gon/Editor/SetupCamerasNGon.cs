3154d3369253a3baf1a4b9bfead8d0f2e0b31526
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2019-10-14 16:04:33 -0400

    Adds the org.janelia.setup-cameras-n-gon package.

---
+++ org.janelia.setup-cameras-n-gon/Editor/SetupCamerasNGon.cs
@@ -0,0 +1,310 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+// Depends on: org.janelia.camera-utilities for OffAxisPerspectiveCamera.
+
+namespace Janelia
+{
+    // If an existing object is used for a screen, it is assumed to be a quad, aligned with the XY plane,
+    // with unit size (i.e., going from -0.5 to 0.5).
+
+    public class SetupCamerasNGon : EditorWindow
+    {
+        private GameObject _fly;
+        private string _flyComponentExtra;
+
+        [Serializable]
+        private class CameraScreen
+        {
+            public Camera camera;
+            public GameObject screen;
+        }
+        private List<CameraScreen> _cameraScreens;
+
+        // The N in the N-gon is _numCameras + _numEmptySides.
+
+        private int _numCameras = 4;
+        private int _numEmptySides = 1;
+        private float _screenWidth = 95.0f;
+        private float _screenHeight = 182.0f;
+        private float _fractionalHeight = 0.3333f;
+        private float _offsetX = 0;
+        private float _offsetZ = 0;
+        private float _near = 0.1f;
+        private float _far = 1000.0f;
+
+        // With GameObject.CreatePrimitive(), properties like these must be present somewhere
+        // in the code to prevent a crash due to code stripping.  See the note at the bottom here:
+        // https://docs.unity3d.com/ScriptReference/GameObject.CreatePrimitive.html
+
+        private MeshFilter _preventStrippingMeshFilter;
+        private MeshRenderer _preventStrippingMeshRenderer;
+        private BoxCollider _preventStrippingBoxCollider;
+
+        [MenuItem("Window/Setup Cameras, N-gon")]
+        public static void ShowWindow()
+        {
+            SetupCamerasNGon window = (SetupCamerasNGon)GetWindow(typeof(SetupCamerasNGon));
+            if (window._cameraScreens.Count == 0)
+            {
+                window.Load();
+            }
+        }
+
+        public SetupCamerasNGon()
+        {
+            _cameraScreens = new List<CameraScreen>();
+        }
+
+        private void OnGUI()
+        {
+            EditorGUILayout.BeginVertical();
+
+            _fly = (GameObject)EditorGUILayout.ObjectField("Fly", _fly, typeof(GameObject), true);
+
+            // A script listed here (without the ".cs" suffix) will be added to the created "fly" object,
+            // as a convenience.
+            _flyComponentExtra = EditorGUILayout.TextField("Extra script for fly", _flyComponentExtra);
+
+            _numCameras = EditorGUILayout.IntField("Number of cameras", _numCameras);
+            _numEmptySides = EditorGUILayout.IntField("Number of empty sides", _numEmptySides);
+
+            _screenWidth = EditorGUILayout.FloatField("Screen width (mm)", _screenWidth);
+            _screenHeight = EditorGUILayout.FloatField("Screen height (mm)", _screenHeight);
+            _fractionalHeight = EditorGUILayout.FloatField("Fractional height", _fractionalHeight);
+
+            // The created "fly" object will be displaced from the center of the n-gon by this vector.
+            _offsetX = EditorGUILayout.FloatField("Offset X (mm)", _offsetX);
+            _offsetZ = EditorGUILayout.FloatField("Offset Z (mm)", _offsetZ);
+
+            _near = EditorGUILayout.FloatField("Near", _near);
+            _far = EditorGUILayout.FloatField("Far", _far);
+
+             while (_cameraScreens.Count < _numCameras)
+            {
+                _cameraScreens.Add(new CameraScreen());
+            }
+
+            for (int i = 0; i < _cameraScreens.Count; i++)
+            {
+                _cameraScreens[i].camera = (Camera)EditorGUILayout.ObjectField("Camera " + (i + 1), _cameraScreens[i].camera, typeof(Camera), true);
+                _cameraScreens[i].screen = (GameObject)EditorGUILayout.ObjectField("Screen " + (i + 1), _cameraScreens[i].screen, typeof(GameObject), true);
+            }
+
+            if (GUILayout.Button("Update"))
+            {
+                UpdateCameras();
+            }
+
+            EditorGUILayout.EndVertical();
+        }
+
+        private void OnDestroy()
+        {
+            Save();
+        }
+
+        private void UpdateCameras()
+        {
+            // TODO: Make the actions here undoable
+            // https://docs.unity3d.com/ScriptReference/Undo.html
+
+            if (_cameraScreens.Count != _numCameras)
+            {
+                Debug.Log("Exactly " + _numCameras + " camera/screen pairs are expected.");
+                return;
+            }
+
+            // Create the objects if they are not specified.
+
+            if (_fly == null)
+            {
+                _fly = new GameObject("Fly");
+                _fly.transform.localPosition = new Vector3(0, 0, 0);
+
+                if ((_flyComponentExtra != null) && (_flyComponentExtra.Length > 0))
+                {
+                    string fullName = _flyComponentExtra + ",Assembly-CSharp";
+                    Type t = Type.GetType(fullName);
+                    if (t != null)
+                    {
+                        _fly.AddComponent(t);
+                    }
+                    else
+                    {
+                        Debug.Log("Cannot find extra script of type '" + fullName + "'");
+                    }
+                }
+            }
+
+            for (int i = 0; i < _cameraScreens.Count; i++)
+            {
+                if (_cameraScreens[i].camera == null)
+                {
+                    GameObject cameraObj = new GameObject("FlyCamera" + (i + 1));
+                    _cameraScreens[i].camera = cameraObj.AddComponent(typeof(Camera)) as Camera;
+
+                    _cameraScreens[i].camera.targetDisplay = i + 1;
+                }
+                _cameraScreens[i].camera.transform.localRotation = Quaternion.identity;
+                _cameraScreens[i].camera.transform.localPosition = Vector3.zero;
+                if (_cameraScreens[i].screen == null)
+                {
+                    _cameraScreens[i].screen = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                    _cameraScreens[i].screen.name = "FlyCamera" + (i + 1) + "Screen";
+                }
+                _cameraScreens[i].screen.transform.localRotation = Quaternion.identity;
+                _cameraScreens[i].screen.transform.localPosition = Vector3.zero;
+
+
+                OffAxisPerspectiveCamera offAxisPerspectiveCamera =
+                    _cameraScreens[i].camera.gameObject.AddComponent<OffAxisPerspectiveCamera>() as OffAxisPerspectiveCamera;
+                offAxisPerspectiveCamera.screen = _cameraScreens[i].screen;
+            }
+
+            // If displays are rotated 90 degrees, so the image is taller than it is wide,
+            // the Windows Display settings should indicate that the displays are in "Portrait" mode
+            // (or "Portrait, flipped"). Then the text displayed by Windows will have the correct
+            // orientation. Unity's Screen.resolutions is aware of this Windows Display setting.
+            // So when the editor is placed on a screen that has been rotated and set to "Portrait",
+            // then Screen.resolutions returns values with height greater than width.
+
+            int numSides = _numCameras + _numEmptySides;
+            float fovDeg = 360.0f / numSides;
+            float fovRad = Mathf.PI * 2.0f / numSides;
+
+            float camYRot = 0.0f;
+
+            float viewDirTrans = (_screenWidth / 2.0f) / Mathf.Tan(fovRad / 2.0f);
+            float heightDirTrans = _screenHeight / 2.0f - _fractionalHeight * _screenHeight;
+
+            for (int i = 0; i < _cameraScreens.Count; i++)
+            {
+                Transform cameraXform = _cameraScreens[i].camera.gameObject.transform;
+                cameraXform.SetParent(_fly.transform);
+
+                Transform screenXform = _cameraScreens[i].screen.transform;
+                screenXform.SetParent(cameraXform);
+
+                _cameraScreens[i].camera.nearClipPlane = _near;
+                _cameraScreens[i].camera.farClipPlane = _far;
+
+                cameraXform.localPosition = new Vector3(0, 0, 0);
+                cameraXform.Rotate(0, camYRot, 0);
+
+                screenXform.localPosition = new Vector3(0, heightDirTrans, viewDirTrans);
+                screenXform.localScale = new Vector3(_screenWidth, _screenHeight, 1);
+
+                // One way of making the screens invisible to their cameras.
+                screenXform.Rotate(0, 180, 0);
+
+                screenXform.position += new Vector3(_offsetX, 0, _offsetZ);
+
+                camYRot += fovDeg;
+            }
+        }
+
+        // Storing the state across sessions, using resources.
+
+        private CamerasNGonSaved _saved;
+
+        private void Save()
+        {
+            _saved.flyName = PathName(_fly);
+            _saved.cameraNames.Clear();
+            _saved.screenNames.Clear();
+            foreach (CameraScreen cameraScreen in _cameraScreens)
+            {
+                GameObject cameraObj = (cameraScreen.camera != null) ? cameraScreen.camera.gameObject : null;
+                _saved.cameraNames.Add(PathName(cameraObj));
+                _saved.screenNames.Add(PathName(cameraScreen.screen));
+            }
+            _saved.numEmptySides = _numEmptySides;
+            _saved.screenWidth = _screenWidth;
+            _saved.screenHeight = _screenHeight;
+            _saved.fractionalHeight = _fractionalHeight;
+            _saved.near = _near;
+            _saved.far = _far;
+
+            AssetDatabase.Refresh();
+            EditorUtility.SetDirty(_saved);
+            AssetDatabase.SaveAssets();
+        }
+
+        private void Load()
+        {
+            _saved = Resources.Load<CamerasNGonSaved>("Editor/savedCamerasNGon");
+            if (_saved != null)
+            {
+                _fly = GameObject.Find(_saved.flyName);
+                if (_saved.cameraNames.Count == _saved.screenNames.Count)
+                {
+                    _cameraScreens.Clear();
+                    for (int i = 0; i < _saved.cameraNames.Count; i++)
+                    {
+                        CameraScreen cameraScreen = new CameraScreen();
+                        GameObject cameraObj = GameObject.Find(_saved.cameraNames[i]);
+                        cameraScreen.camera = (cameraObj != null) ? cameraObj.GetComponent<Camera>() : null;
+                        cameraScreen.screen = GameObject.Find(_saved.screenNames[i]);
+                        _cameraScreens.Add(cameraScreen);
+                    }
+                    _numCameras = _cameraScreens.Count;
+                    _numEmptySides = _saved.numEmptySides;
+                    _screenWidth = _saved.screenWidth;
+                    _screenHeight = _saved.screenHeight;
+                    _fractionalHeight = _saved.fractionalHeight;
+                    _near = _saved.near;
+                    _far = _saved.far;
+                }
+            }
+            else
+            {
+                _saved = CreateInstance<CamerasNGonSaved>();
+
+                string root = Application.dataPath;
+                EnsureDirectory(root + "/Resources");
+                EnsureDirectory(root + "/Resources/Editor");
+
+                // Saving and loading work only if the filename has the extension ".asset".
+
+                AssetDatabase.CreateAsset(_saved, "Assets/Resources/Editor/savedCamerasNGon.asset");
+                AssetDatabase.SaveAssets();
+                AssetDatabase.Refresh();
+            }
+        }
+
+        private void EnsureDirectory(string path)
+        {
+            if (!Directory.Exists(path))
+            {
+                try
+                {
+                    Directory.CreateDirectory(path);
+                }
+                catch (Exception e)
+                {
+                    Debug.Log("Cannot create " + path + ": " + e.ToString());
+                }
+            }
+        }
+
+        private string PathName(GameObject o)
+        {
+            if (o == null)
+            {
+                return "";
+            }
+            string path = o.name;
+            while (o.transform.parent != null)
+            {
+                o = o.transform.parent.gameObject;
+                path = o.name + "/" + path;
+            }
+            return path;
+        }
+    }
+}
---
+++ org.janelia.setup-cameras-n-gon/README.md
@@ -0,0 +1,9 @@
+# Janelia Setup Cameras N-Gon
+
+## Summary
+This package (org.janelia.setup-cameras-n-gon) implements an editor to set up a rig of cameras arranged in an N-gon (e.g., a pentagon), optionally with one or more of the sides left "open" (without associated cameras).
+
+## Installation
+Follow the [installation instructions in the main repository](https://github.com/JaneliaSciComp/janelia-unity-toolkit/blob/master/README.md#installation).
+
+Beware that the installation will fail unless the package [org.janelia.camera-utilites](https://github.com/JaneliaSciComp/janelia-unity-toolkit/tree/master/org.janelia.camera-utilities) has been installed already.  Unity does not support automatic installation of dependencies like this one when they are "local packages" cloned from GitHub (as of Unity version 2019.2.7f2, at least).
