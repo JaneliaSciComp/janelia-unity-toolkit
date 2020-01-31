f866e6fb1715ef42fb03f934920791c5d9a2dea2
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2020-01-31 15:51:44 -0500

    Adds org.janelia.collision-handling.

---
+++ org.janelia.collision-handling/README.md
@@ -0,0 +1,56 @@
+# Janelia Collision Handling
+
+## Summary
+This package (org.janelia.collision-handling) adds some functionality for collision detection and response.
+
+An example is the `KinematicCollisionHandler` class, which adds simple collision handling for a Transform moving kinematically, like the Transform from a GameObject representing a fly walking on a treadmill.  The following example code shows how to use this class:
+
+```csharp
+using UnityEngine;
+
+public class ButtonMoveable : MonoBehaviour
+{
+    private Janelia.KinematicCollisionHandler _collisionHandler;
+
+    void Start()
+    {
+        // Set up the collision handler to act on this GameObject's transform.
+        _collisionHandler = new Janelia.KinematicCollisionHandler(transform);
+    }
+
+    void Update()
+    {
+        // Let the user translate the transform with arrow keys.
+        float delta = 0.225f;
+        Vector3 translation = default;
+        if (Input.GetKeyDown("left"))
+        {
+            translation = new Vector3(-delta, 0, 0);
+        }
+        if (Input.GetKeyDown("right"))
+        {
+            translation = new Vector3(delta, 0, 0);
+        }
+        if (Input.GetKeyDown("up"))
+        {
+            translation = new Vector3(0, 0, delta);
+        }
+        if (Input.GetKeyDown("down"))
+        {
+            translation = new Vector3(0, 0, -delta);
+        }
+
+        if (translation != default)
+        {
+            // Let the collision handler correct the translation, with approximated sliding contact,
+            // and apply it to this GameObject's transform.  The corrected translation is returned.
+            Vector3 actualTranslation = _collisionHandler.Translate(translation);
+
+            Debug.Log("frame " + Time.frameCount + ": translation " + translation + " becomes " + actualTranslation);
+        }
+    }
+}
+```
+
+## Installation
+Follow the [installation instructions in the main repository](https://github.com/JaneliaSciComp/janelia-unity-toolkit/blob/master/README.md#installation).
---
+++ org.janelia.collision-handling/Runtime/KinematicCollisionHandler.cs
@@ -0,0 +1,65 @@
+﻿// Simple collision detection and response for a Transform moving kinematically
+// (e.g., the Transform from a GameObject representing a fly walking on a treadmill
+// to move through a virtual world).  Collisions with all Colliders in the scene 
+// will be handled (and note that most standard 3D objects have a Collider by
+// default).  The algorithms treat the Transform's object as a small sphere and
+// support an approximate form of sliding along the surface of any contacted Collider.
+
+using UnityEngine;
+
+namespace Janelia
+{
+    public class KinematicCollisionHandler
+    {
+        // Collisions are calculated for a sphere of this radius (which should be small).
+
+        public float radius;
+
+        // Initialize collision handling for the specified Transform, represented
+        // as a sphere with the specified radius.
+
+        public KinematicCollisionHandler(Transform transf, float rad = 0.1f)
+        {
+            radius = rad;
+            _transform = transf;
+        }
+
+        // Correct the specified translation to account for collisions, and apply it to
+        // this handler's Transform.  Returns the corrected translation.
+
+        public Vector3 Translate(Vector3 translation, bool approximateSliding = true)
+        {
+            Vector3 validTranslation = translation;
+
+            Ray ray = new Ray(_transform.position, translation);
+            float maxDistance = translation.magnitude + radius;
+            RaycastHit[] hits = Physics.RaycastAll(ray, maxDistance);
+            if (hits.Length > 0)
+            {
+                float closestDistance = 2.0f * maxDistance;
+                RaycastHit closestHit = default;
+                foreach (RaycastHit hit in hits)
+                {
+                    if (hit.distance < closestDistance)
+                    {
+                        closestDistance = hit.distance;
+                        closestHit = hit;
+                    }
+                }
+                float contactDistance = (closestHit.point - _transform.position).magnitude;
+                contactDistance -= radius;
+                validTranslation = contactDistance * translation.normalized;
+
+                if (approximateSliding && (Mathf.Abs(contactDistance) < radius / 1000.0f))
+                {
+                    validTranslation = Vector3.ProjectOnPlane(translation, closestHit.normal);
+                }
+            }
+
+            _transform.Translate(validTranslation);
+            return validTranslation;
+        }
+
+        private Transform _transform;
+    }
+}
