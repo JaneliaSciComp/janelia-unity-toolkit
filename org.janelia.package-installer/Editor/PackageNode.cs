b72739df8fe94c0f60a5b259e6e7216a70e1bb0e
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2020-10-15 06:45:22 -0400

    Adds the org.janelia.package-installer package.

---
+++ org.janelia.package-installer/Editor/PackageNode.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+namespace Janelia
+{
+    public class PackageNode
+    {
+        public readonly string PkgDir;
+        public readonly HashSet<PackageNode> Deps;
+
+        public PackageNode(string pkgDir)
+        {
+            _pkgDirToNode.Add(pkgDir, this);
+
+            PkgDir = pkgDir;
+
+            HashSet<string> depPkgDirs = GetDepPkgDirs(pkgDir, "Runtime");
+            depPkgDirs.UnionWith(GetDepPkgDirs(pkgDir, "Editor"));
+
+            Deps = new HashSet<PackageNode>();
+            foreach (string depPkgDir in depPkgDirs)
+            {
+                // Do not get into a cycle if "Editor" depends on "Runtime".
+                if (depPkgDir == pkgDir)
+                {
+                    continue;
+                }
+
+                // An edge from node U to node V means U should preceed V in the
+                // topological sort, that is, V has a dependency on U and
+                // U must be installed before V.
+                PackageNode other = _pkgDirToNode.ContainsKey(depPkgDir) ?
+                    _pkgDirToNode[depPkgDir] :
+                    new PackageNode(depPkgDir);
+                other.Deps.Add(this);
+            }
+        }
+
+        static public List<string> TopoSort()
+        {
+            List<string> result = new List<string>();
+
+            // Depth-first search gives the topological sort in reversed order.
+            // So do the search and then reverse the result.
+            foreach (PackageNode node in _pkgDirToNode.Values)
+            {
+                Visit(node);
+            }
+            result.Reverse();
+
+            // Get ready for the next use.
+            _pkgDirToNode.Clear();
+
+            return result;
+
+            void Visit(PackageNode node, int depth = 0)
+            {
+                // The linear performance of `Contains` is not an issue since `result` will never get large.
+                if (!result.Contains(node.PkgDir))
+                {
+                    // Avoid catestrophic failures that would affect the user interface.
+                    if (depth > 128)
+                    {
+                        Debug.Log("Aborted recursion in PackageNode at depth " + depth);
+                        return;
+                    }
+
+                    foreach (PackageNode dep in node.Deps)
+                    {
+                        Visit(dep, depth + 1);
+                    }
+                    result.Add(node.PkgDir);
+                }
+            }
+        }
+
+        private HashSet<string> GetDepPkgDirs(string pkgDir, string subDir)
+        {
+            HashSet<string> result = new HashSet<string>();
+            string rootDir = Path.GetDirectoryName(pkgDir);
+            string dir = pkgDir + Path.DirectorySeparatorChar + subDir;
+            if (System.IO.Directory.Exists(dir))
+            {
+                string[] asmdefPaths = System.IO.Directory.GetFiles(dir, "*.asmdef");
+                foreach (string asmdefPath in asmdefPaths)
+                {
+                    string asmdefContents = System.IO.File.ReadAllText(asmdefPath);
+                    Asmdef asmdef = JsonUtility.FromJson<Asmdef>(asmdefContents);
+                    foreach (string reference in asmdef.references)
+                    {
+                        int i = reference.LastIndexOf(".");
+                        string referencePkg = reference.Substring(0, i).ToLower();
+                        string referencePkgDir = rootDir + Path.DirectorySeparatorChar + "org." + referencePkg;
+                        result.Add(referencePkgDir);
+                    }
+                }
+            }
+            return result;
+        }
+
+        // A struct with the part of the .asmdef JSON file that needs to be parsed to determine dependencies.
+        [Serializable]
+        private struct Asmdef
+        {
+            public string[] references;
+
+            // This constructor prevents a spurious compiler warning about `references` never getting assigned.
+            Asmdef(string[] refs = null)
+            {
+                references = refs;
+            }
+        };
+
+        private static Dictionary<string, PackageNode> _pkgDirToNode = new Dictionary<string, PackageNode>();
+    }
+}
---
+++ org.janelia.package-installer/Tests/Editor/PackageNodeTest.cs
@@ -0,0 +1,154 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace Janelia
+{
+    public static class PackageNodeTest
+    {
+        [Test]
+        public static void Test1()
+        {
+            char Sep = Path.DirectorySeparatorChar;
+            string pathRoot = Path.GetTempPath() + "Test1";
+            DirectoryInfo diRoot = Directory.CreateDirectory(pathRoot);
+
+            // A uses nothing
+            // B uses A
+
+            setupDependency(pathRoot, "a/Runtime", new string[] { });
+            setupDependency(pathRoot, "b/Runtime", new string[] { "A.Runtime" });
+
+            PackageNode nodeB = new PackageNode(pathRoot + Sep + "org.janelia.b");
+            List<string> topoSort = PackageNode.TopoSort();
+
+            Assert.AreEqual(2, topoSort.Count);
+            Assert.AreEqual("org.janelia.a", Path.GetFileName(topoSort[0]));
+            Assert.AreEqual("org.janelia.b", Path.GetFileName(topoSort[1]));
+
+            diRoot.Delete(true);
+        }
+
+        [Test]
+        public static void Test2()
+        {
+            char Sep = Path.DirectorySeparatorChar;
+            string pathRoot = Path.GetTempPath() + "Test2";
+            DirectoryInfo diRoot = Directory.CreateDirectory(pathRoot);
+
+            // A uses nothing
+            // B uses A
+            // C uses A, B
+
+            setupDependency(pathRoot, "a/Runtime", new string[] { });
+            setupDependency(pathRoot, "b/Runtime", new string[] { "A.Runtime" });
+            setupDependency(pathRoot, "c/Runtime", new string[] { "A.Runtime", "B.Runtime" });
+
+            PackageNode nodeC = new PackageNode(pathRoot + Sep + "org.janelia.c");
+            List<string> topoSort = PackageNode.TopoSort();
+
+            Assert.AreEqual(3, topoSort.Count);
+            Assert.AreEqual("org.janelia.a", Path.GetFileName(topoSort[0]));
+            Assert.AreEqual("org.janelia.b", Path.GetFileName(topoSort[1]));
+            Assert.AreEqual("org.janelia.c", Path.GetFileName(topoSort[2]));
+
+            diRoot.Delete(true);
+        }
+
+        [Test]
+        public static void Test3()
+        {
+            char Sep = Path.DirectorySeparatorChar;
+            string pathRoot = Path.GetTempPath() + "Test3";
+            DirectoryInfo diRoot = Directory.CreateDirectory(pathRoot);
+
+            // A's Editor uses nothing
+            // B's Editor uses B's Runtime, A's Editor
+            // C's Runtime uses B's Runtime
+
+            setupDependency(pathRoot, "a/Editor", new string[] { });
+            setupDependency(pathRoot, "b/Editor", new string[] { "B.Runtime", "A.Editor" });
+            setupDependency(pathRoot, "c/Runtime", new string[] { "B.Runtime" });
+
+            PackageNode nodeC = new PackageNode(pathRoot + Sep + "org.janelia.c");
+            List<string> topoSort = PackageNode.TopoSort();
+
+            Assert.AreEqual(3, topoSort.Count);
+            Assert.AreEqual("org.janelia.a", Path.GetFileName(topoSort[0]));
+            Assert.AreEqual("org.janelia.b", Path.GetFileName(topoSort[1]));
+            Assert.AreEqual("org.janelia.c", Path.GetFileName(topoSort[2]));
+
+            diRoot.Delete(true);
+        }
+
+        [Test]
+        public static void Test4()
+        {
+            char Sep = Path.DirectorySeparatorChar;
+            string pathRoot = Path.GetTempPath() + "Test4";
+            DirectoryInfo diRoot = Directory.CreateDirectory(pathRoot);
+
+            // C uses A
+            // D uses A
+            // E uses A, B
+            // F uses C, D
+            // G use E
+            // H uses A, F, G
+
+            setupDependency(pathRoot, "a/Runtime", new string[] { });
+            setupDependency(pathRoot, "b/Runtime", new string[] { });
+            setupDependency(pathRoot, "c/Runtime", new string[]{ "A.Runtime" });
+            setupDependency(pathRoot, "d/Runtime", new string[] { "A.Runtime" });
+            setupDependency(pathRoot, "e/Runtime", new string[] { "A.Runtime", "B.Runtime" });
+            setupDependency(pathRoot, "f/Runtime", new string[] { "C.Runtime", "D.Runtime" });
+            setupDependency(pathRoot, "g/Runtime", new string[] { "E.Runtime" });
+            setupDependency(pathRoot, "h/Runtime", new string[] { "A.Runtime", "F.Runtime", "G.Runtime" });
+
+            PackageNode nodeH = new PackageNode(pathRoot + Sep + "org.janelia.h");
+            List<string> topoSort = PackageNode.TopoSort();
+
+            string pre = pathRoot + Sep + "org.janelia.";
+
+            Assert.AreEqual(8, topoSort.Count);
+            Assert.Greater(topoSort.IndexOf(pre + "c"), topoSort.IndexOf(pre + "a"));
+            Assert.Greater(topoSort.IndexOf(pre + "d"), topoSort.IndexOf(pre + "a"));
+            Assert.Greater(topoSort.IndexOf(pre + "e"), topoSort.IndexOf(pre + "a"));
+            Assert.Greater(topoSort.IndexOf(pre + "e"), topoSort.IndexOf(pre + "b"));
+            Assert.Greater(topoSort.IndexOf(pre + "f"), topoSort.IndexOf(pre + "c"));
+            Assert.Greater(topoSort.IndexOf(pre + "f"), topoSort.IndexOf(pre + "d"));
+            Assert.Greater(topoSort.IndexOf(pre + "g"), topoSort.IndexOf(pre + "e"));
+            Assert.Greater(topoSort.IndexOf(pre + "h"), topoSort.IndexOf(pre + "a"));
+            Assert.Greater(topoSort.IndexOf(pre + "h"), topoSort.IndexOf(pre + "f"));
+            Assert.Greater(topoSort.IndexOf(pre + "h"), topoSort.IndexOf(pre + "g"));
+
+            diRoot.Delete(true);
+        }
+
+        private static void setupDependency(string pathRoot, string pkgAndFolder, string[] dependentOn)
+        {
+            char Sep = Path.DirectorySeparatorChar;
+
+            string[] pkgAndFolderSplit = pkgAndFolder.Split('/');
+            Assert.AreEqual(2, pkgAndFolderSplit.Length);
+            string pkg = pkgAndFolderSplit[0];
+            string folder = pkgAndFolderSplit[1];
+
+            Directory.CreateDirectory(pathRoot + Sep + "org.janelia." + pkg);
+            Directory.CreateDirectory(pathRoot + Sep + "org.janelia." + pkg + Sep + folder);
+
+            string asmdefDeps = "";
+            foreach (string dep in dependentOn)
+            {
+                if (asmdefDeps.Length > 0)
+                    asmdefDeps += ", ";
+                asmdefDeps += "\"Janelia." + dep + "\"";
+            }
+
+            string nameAsmdef = "Janelia." + pkg.ToUpper() + "." + folder + ".asmdef";
+            string pathAsmdef = pathRoot + Sep + "org.janelia." + pkg + Sep + folder + Sep + nameAsmdef;
+            File.WriteAllText(pathAsmdef, "{ \"references\": [ " + asmdefDeps + " ] }");
+        }
+    }
+}
