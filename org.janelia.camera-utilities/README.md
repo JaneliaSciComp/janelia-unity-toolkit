59fa903aa3ca449acd756db14e3a46b214cea523
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2024-02-23 12:41:13 -0500

    Adds rendering of panoramas for freely moving viewpoints.

--- org.janelia.camera-utilities/README.md
+++ org.janelia.camera-utilities/README.md
@@ -9,17 +9,17 @@ Follow the [installation instructions in the main repository](https://github.com
 
 ## Details
 
-### Janelia.OffAxisPerspectiveCamera
+### `Janelia.OffAxisPerspectiveCamera`
 
 Makes the attached camera use off-axis perspective projection.
 
-### Janelia.AdjoiningDisplaysCamera and Janelia.AdjoiningDisplaysCameraBuilder
+### `Janelia.AdjoiningDisplaysCamera` and `Janelia.AdjoiningDisplaysCameraBuilder`
 
 Unity supports ["multi-display"](https://docs.unity3d.com/Manual/MultiDisplay.html), with a game having multiple cameras each shown on its own external display monitor.  Framerates above 60 Hz are not possible, though (as of 2019).
 
 The `Janelia.AdjoiningDisplaysCamera` script implements an alternative with higher performance.  It combines the multiple camera images into one wide image that is associated with another, main camera.  The associated `Janelia.AdjoiningDisplaysCameraBuilder` builds a standalone executable with the special options to make the main camera's wide image extend across all the external displays, putting the appropriate part on each display.
 
-To use `Janelia.AdjoiningDisplaysCamera` on Windows:
+To use `AdjoiningDisplaysCamera` on Windows:
 
 1. Connect the external displays so they are numbered `2` through `N` in the Windows "Display Settings".  It is best if `2` is the left-most, and then the display numbers increase in order.  But if such an arrangement is not possible, see step 7., below.
 
@@ -49,8 +49,39 @@ Also, pressing the `m` key toggles mirroring of the displays on and off (useful 
 
 ### Frame Packing
 
-For animal participants who can see very fast changes (e.g., the _Drosophila_ fruit fly), the `AdjoiningDisplaysCamera` supports an optional way of improving the visual smoothness through "frame packing" for DLP (digital light processing) projectors.   When ready to render frame _i_ at time _t\_i_, `AdjoiningDisplaysCamera` interpolates the camera pose (position, orientation) at three fractions of the interval from _t\_i-1_ to _t\_i_, and renders the scene at each fraction.  It then packs the resulting image from each fraction into one color channel of the image that is finally displayed.  [A DLP displays each color channel successively](https://www.benq.com/en-us/business/resource/trends/dlp-and-3lcd-projectors.html) for a fraction of the overall frame time, so the net effect is that the images for the interpolated camera poses are visible as frames at a higher frame rate to animals capable of seeing at the higher rate.  The higher-rate frames appear in grayscale instead of color, but that is acceptable in some applications.
+For animal participants who can see very fast changes (e.g., the _Drosophila_ fruit fly), the `Janelia.AdjoiningDisplaysCamera` supports an optional way of improving the visual smoothness through "frame packing" for DLP (digital light processing) projectors.   When ready to render frame _i_ at time _t\_i_, `AdjoiningDisplaysCamera` interpolates the camera pose (position, orientation) at three fractions of the interval from _t\_i-1_ to _t\_i_, and renders the scene at each fraction.  It then packs the resulting image from each fraction into one color channel of the image that is finally displayed.  [A DLP displays each color channel successively](https://www.benq.com/en-us/business/resource/trends/dlp-and-3lcd-projectors.html) for a fraction of the overall frame time, so the net effect is that the images for the interpolated camera poses are visible as frames at a higher frame rate to animals capable of seeing at the higher rate.  The higher-rate frames appear in grayscale instead of color, but that is acceptable in some applications.
 
 The Unity Editor's Inspector gives control over what the three fractions are, and what color channel corresponds to each fraction.  The fractions are controlled with three 0-to-1 sliders.  What color channel corresponds to each fraction is controled by a "packing order" value; a packing order of "GRB", for example, indicates that the first fraction corresponds to the display of green, the second fraction to red and the third to blue.
 
 Note that while frame packing increases effective frame rate and smoothness, it does _not_ reduce latency.  In fact, it actually increases the latency by roughly the time to draw one frame.
+
+### Panoramic Display on Non-Flat Screens
+
+The `Janelia.PanoramicDisplayCamera` script supports displays that are not traditional flat surfaces. Supported alternatives include a cylindrical display surface surrounded by a ring of projectors, "back projecting" the panorama visible to a subject moving freely inside the cylinder. This script sets up a box of cameras pointing out from the subject, and then remaps those camera views to the format needed by the projectors in real time using the GPU.
+
+The `Janelia.ExampleUsingPanoramicDisplayCamera` script shows how to set up the data needed for a cylindrical display, which is passed to `PanoramicDisplayCamera` with its `SetDisplaySurfaceData` function. The tracking of the freely moving subject (e.g., using computer vision) must be implemented separately.
+
+The curvature of a display surface like a cylinder may distort the brightness of the projected image (e.g., reducing the brightness at the borders between projectors, since those areas have more of an angle away from projector light source). A compensating texture (e.g., with a reduction factor applied away from the borders between projectors) can be passed to `PanoramicDisplayCamera`. The following image shows the effect for three projectors, with the compensation exagerated for emphasis; note that there are three regions across the image with gradual lightening at the edges of each region.
+
+<p align="center">
+<img src="./panoramicDisplayCamera.PNG" height="400">
+</p>
+
+
+This compensating texture can be generated with the `Janelia.CameraUtilities.SetupCylinderProjectorEdgeBrightener` function, which computes computes the brightness attenuation with [Lambert's cosine law](https://en.wikipedia.org/wiki/Lambert%27s_cosine_law); it also includes a user-defined scale factor, that can be changed interactively to match the conditions of particular display surface. See the `ExampleUsingPanoramicDisplayCamera` script. An alternative is to write a custom function to generate the compensating texture, using the `CameraUtilities.SetupCylinderProjectorMaskDelegate` API.
+
+Screen curvature can also distort the color near the borders between projectors, so `PanoramicDisplayCamera` also takes a color correction texture. This correction texture can be generated with the `Janelia.CameraUtilities.SetupCylinderProjectorEdgeColorCorrector` function, which also uses the cosine law and includes user-defined parameters for the color to correct and for scaling.
+
+Like `AdjoiningDisplaysCamera` described above, `PanoramicDisplayCamera` generates a single image that adjoins the images for each projector horizontally. To use this image on Windows, go to the Windows "Display Settings and arrange the projectors in order, left to right; then the image can extend across all the projectors, with the appropriate part on each projector.
+
+There are some constraints on the projectors and the cylindrical screen, so that projectors with a specified (horizontal) field of view can cover a cylinder of a specified radius with the adjoined images. The `PanoramicDisplayCamera.SetDisplaySurfaceData` function computes and prints out these constraints as it runs, so they appear in either the Unity editor console, or the `Player.log` text file, from the
+[place where Unity stores its logs](https://docs.unity3d.com/Manual/LogFiles.html). Example output is:
+```
+Please position projector 0 at (-9.97, 0.00, -5.76)
+Please position projector 1 at (0.00, 0.00, 11.52)
+Please position projector 2 at (9.97, 0.00, -5.76)
+Please make the cylinder 5.100435 units tall.
+```
+
+
+Using a display surface other than a cylinder involves calling `PanoramicDisplayCamera.SetDisplaySurfaceData` with custom arrays that describe where the pixels of the projectors map to 3D positions on the display surface. Custom arrays are also useful for a cylindrical display surface if there is to be regions of overlap between the images from adjacent projectors.
---
+++ org.janelia.camera-utilities/Runtime/PanoramicDisplayCamera.cs
@@ -0,0 +1,322 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+
+// Renders panoramas in real time from a freely moving viewpoint. The viewpoint comes from the `Transform`
+// for the `GameObject` this script is attached to. The panoramic rendering involves rendering a box of
+// views around the viewpoint---north, south, east, west, down, and optionally up---and then using a custom
+// shader (`Assets/Resources/PanoramicDisplay.shader`) to remap those renderings into the panorama. The
+// remapping is specific to the geometry of the display surface (e.g., a cylinder around the viewpoint back-
+// projected onto by three external projectors, or a hemisphere projected onto by one overhead projector, etc.).
+// The details of this geometry is passed to this script as the application is starting. Additional textures
+// to correct the brightness and color of the images on the display surfaces can also be specified.
+
+namespace Janelia
+{
+    public class PanoramicDisplayCamera : MonoBehaviour
+    {
+        // The cameras that renderig the box around the viewpoint.
+        public Camera[] sourceCameras = new Camera[6];
+
+        // The width of each of the source cameras. The height is the same, as each side of the box is a square.
+        public int sourceWidth = 512;
+
+        // Scaling factors for the masking (e.g., to give brightness compensation) and the color correction.
+        // These scaling factors can be changed as the application is running.  See the comments for
+        // `SetDisplaySurfaceData`, below, and also in `ExampleUsingPanoramiceDisplayCamera.cs`.
+        public float surfaceMaskScale = 1;
+        public float surfaceColorCorrectionScale = 0;
+
+        // If the panorama is to be displayed with more than one external display or projector, the images for
+        // all the displays are adjoined horizontally into a single wide image, and this image "bleeds" from
+        // the left projector onto the others, to the right.
+        public int leftProjectorIndex = 2;
+        public static int leftProjectorIndexStatic = 2;
+
+        // A client should call this function before any `Update` functions are called (e.g. call it in `Start`).
+        // The arrays are a `dataWidth` by `dataHeight` grid of elements in row-major order, sized to match the
+        // pixels on the final display projector, or projectors; the data for multiple projectors must be concatenated
+        // into each array in the left-right order of the projectors (e.g., the order on the Windows Extended Desktop).
+        // The `surfaceXData`, `surfaceYData` and `surfaceZData` arrays define a 3D point for each projector pixel,
+        // with that 3D point being where the pixel is projected on the physical display surface.
+        // The `surfaceMaskData` array has a 0-255 value for each projector pixel, interpreted as a 8-bit float, which 
+        // works with the `surfaceMaskScale` scalar value to adjust the final color for each projector pixel.
+        // Specifically, `c1 = c0 * (1 - surfaceMaskScale * s1)`, where `c1` is the adjusted color, `c0` is the
+        // unadjusted color, and `s1` is the value of the `surfaceMaskData` at the pixel.  So to get a simple "on/off" 
+        // mask, use `surfaceMaskScale` of 1, and set `s1` to 0 for "on" (color unchanged) and 255 for "off" (black).
+        // The `surfaceColorCorrectionData` array has a `Color` for each projector pixel, which works with the 
+        // `surfaceColorCorrectionScale` scalar value to further adjuest the final color for each projector pixels.
+        // Specifically, `c2 = c1 * (1 - surfaceColorCorrectionScale * s2)`, where `c2` is the compensated color, and
+        // `s2` is the color value in `surfaceColorCorrectionData` at the pixel (so that color is subtracted out).
+        // See `ExampleUsingPanoramicDisplayCamera` for an example of how the mask can be used to add brightness
+        // compensation for a cylindrical display screen, and also an example of applying color correction for this
+        // display screen.
+
+        public void SetDisplaySurfaceData(int dataWidth, int dataHeight, float[] surfaceXData, float[] surfaceYData, float[] surfaceZData, 
+            byte[] surfaceMaskData, Color[] surfaceColorCorrectionData)
+        {
+            if (!SystemInfo.SupportsTextureFormat(TextureFormat.RFloat))
+            {
+                Debug.Log("TextureFormat.RFloat is not supported");
+                return;
+            }
+            if (!SystemInfo.SupportsTextureFormat(TextureFormat.R8))
+            {
+                Debug.Log("TextureFormat.R8 is not supported");
+                return;
+            }
+
+            // The final "false" is important, to turn off full-screen display, so an extra wide image will
+            // spill over onto other displays that are adjacent in the Windows extended desktop.
+            Screen.SetResolution(dataWidth, dataHeight, false);
+
+            SetupMaterial();
+
+            _projectorSurfaceXTexture = new Texture2D(dataWidth, dataHeight, TextureFormat.RFloat, mipChain: false, linear: true);
+            _projectorSurfaceXTexture.SetPixelData(surfaceXData, mipLevel: 0);
+            _projectorSurfaceXTexture.filterMode = FilterMode.Bilinear;
+            _projectorSurfaceXTexture.Apply();
+
+            _material.SetTexture("_TexProjectorSurfaceX", _projectorSurfaceXTexture);
+
+            _projectorSurfaceYTexture = new Texture2D(dataWidth, dataHeight, TextureFormat.RFloat, mipChain: false, linear: true);
+            _projectorSurfaceYTexture.SetPixelData(surfaceYData, mipLevel: 0);
+            _projectorSurfaceYTexture.filterMode = FilterMode.Bilinear;
+            _projectorSurfaceYTexture.Apply();
+
+            _material.SetTexture("_TexProjectorSurfaceY", _projectorSurfaceYTexture);
+
+            _projectorSurfaceZTexture = new Texture2D(dataWidth, dataHeight, TextureFormat.RFloat, mipChain: false, linear: true);
+            _projectorSurfaceZTexture.SetPixelData(surfaceZData, mipLevel: 0);
+            _projectorSurfaceZTexture.filterMode = FilterMode.Bilinear;
+            _projectorSurfaceZTexture.Apply();
+
+            _material.SetTexture("_TexProjectorSurfaceZ", _projectorSurfaceZTexture);
+
+            _projectorSurfaceMaskTexture = new Texture2D(dataWidth, dataHeight, TextureFormat.R8, mipChain: false, linear: true);
+            _projectorSurfaceMaskTexture.SetPixelData(surfaceMaskData, mipLevel: 0);
+            _projectorSurfaceMaskTexture.filterMode = FilterMode.Bilinear;
+            _projectorSurfaceMaskTexture.Apply();
+
+            _material.SetTexture("_TexMask", _projectorSurfaceMaskTexture);
+
+            _projectorSurfaceColorCorrectionTexture = new Texture2D(dataWidth, dataHeight, TextureFormat.ARGB32, mipChain: false, linear: true);
+            _projectorSurfaceColorCorrectionTexture.SetPixels(surfaceColorCorrectionData, miplevel: 0);
+            _projectorSurfaceColorCorrectionTexture.filterMode = FilterMode.Bilinear;
+            _projectorSurfaceColorCorrectionTexture.Apply();
+
+            _material.SetTexture("_TexColorCorrection", _projectorSurfaceColorCorrectionTexture);
+        }
+
+        public void Start()
+        {
+            Camera camera = GetComponent<Camera>();
+            if (camera.targetDisplay != 0)
+            {
+                Debug.LogWarning("PanoramicDisplayCamera: attached camera should have 'Target Display' set to 'Display 1'");
+            }
+
+            // Don't render anything with this camera, as OnRenderImage() will completely replace
+            // its image with the concatenation of the displayCamera images.
+            camera.cullingMask = 0;
+            camera.clearFlags = CameraClearFlags.Nothing;
+
+            SetupSourceCameras(sourceWidth, sourceWidth);
+        }
+
+        public void Update()
+        {
+            _material.SetFloat("_MaskScale", surfaceMaskScale);
+            _material.SetFloat("_ColorCorrectionScale", surfaceColorCorrectionScale);
+        }
+
+        public void OnRenderImage(RenderTexture input, RenderTexture output)
+        {
+            if (!SourceCamerasAreValid() ||!TexturesAreValid())
+            {
+                return;
+            }
+
+            RenderSourceCameras();
+
+            // Do this every frame to suppress the warning:
+            // "OnRenderImage() possibly didn't write anything to the destination texture!"
+            Graphics.SetRenderTarget(output);
+
+            int finalWidth = input.width;
+            int finalHeight = input.height;
+
+            GL.PushMatrix();
+            GL.LoadPixelMatrix(0, finalWidth, finalHeight, 0);
+
+            DrawFromSourceCameraTextures(finalWidth, finalHeight);
+
+            GL.PopMatrix();
+        }
+
+        private void SetupSourceCameras(int width, int height)
+        {
+            int n = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                if (sourceCameras[i] != null)
+                {
+                    sourceCameras[i].targetTexture = new RenderTexture(width, height, 24, RenderTextureFormat.ARGB32);
+                    sourceCameras[i].enabled = false;
+                    ++n;
+                }
+            }
+            _enableSixCameras = (n == 6);
+            if (_enableSixCameras)
+            {
+                _material.EnableKeyword("SIX_CAMERAS");
+            }
+        }
+
+        private void SetupMaterial()
+        {
+            // For the shader `org.janelia.camera-utilities/Assets/Resources/PanoramicDisplay.shader`
+            // the name to use when loading is just `PanoramicDisplay`.
+            Shader shader = Resources.Load("PanoramicDisplay", typeof(Shader)) as Shader;
+            if (shader != null)
+            {
+                _material = new Material(shader);
+            }
+            else
+            {
+                Debug.Log("Could not load PanoramicDisplay.shader");
+            }
+        }
+
+
+        private bool SourceCamerasAreValid()
+        {
+            int specifiedCount = 0;
+            for (int i = 0; i < sourceCameras.Count(); i++)
+            {
+                specifiedCount += (sourceCameras[i] != null) ? 1 : 0;
+            }
+            if (specifiedCount < 5)
+            {
+                Debug.LogWarning("PanoramicDisplayCamera: at least 5 source cameras must be specified");
+                return false;
+            }
+            return true;
+        }
+
+        private bool TexturesAreValid()
+        {
+            if (_projectorSurfaceXTexture == null)
+            {
+                Debug.Log("_projectorSurfaceXTexture is null");
+                return false;
+            }
+            if (_projectorSurfaceYTexture == null)
+            {
+                Debug.Log("_projectorSurfaceYTexture is null");
+                return false;
+            }
+            if (_projectorSurfaceZTexture == null)
+            {
+                Debug.Log("_projectorSurfaceZTexture is null");
+                return false;
+            }
+            if (_projectorSurfaceMaskTexture == null)
+            {
+                Debug.Log("_projectorSurfaceMaskTexture is null");
+                return false;
+            }
+            if (_projectorSurfaceColorCorrectionTexture == null)
+            {
+                Debug.Log("_projectorSurfaceColorCorrectionTexture is null");
+                return false;
+            }
+            return true;
+        }
+
+        private void RenderSourceCameras()
+        {
+            foreach (Camera sourceCamera in sourceCameras)
+            {
+                if (sourceCamera != null)
+                {
+                    sourceCamera.Render();
+                }
+            }
+        }
+
+        private void DrawFromSourceCameraTextures(int finalWidth, int finalHeight)
+        {
+            if (sourceCameras.Count() < 5)
+            {
+                Debug.LogWarning("PanoramicDisplayCamera: expecting at least 5 source cameras instead of " + sourceCameras.Count());
+                return;
+            }
+
+            int n = _enableSixCameras ? 6 : 5;
+            for (int i = 0; i < n; i++)
+            {
+                SetMaterialCamera(_material, sourceCameras[i], i);
+            }
+
+            Graphics.DrawTexture(new Rect(0, 0, finalWidth, finalHeight), sourceCameras[0].targetTexture, _material);
+        }
+
+        private void SetMaterialCamera(Material material, Camera camera, int i)
+        {
+            string baseName = "_Camera" + i.ToString(); 
+            material.SetVector(baseName + "Position", camera.transform.position);
+            material.SetVector(baseName + "Forward", camera.transform.forward);
+            material.SetVector(baseName + "Up", camera.transform.up);
+            material.SetVector(baseName + "Right", camera.transform.right);
+            material.SetFloat(baseName + "Near", camera.nearClipPlane);
+            float fovHoriz = camera.fieldOfView;
+            float fovVert = Camera.VerticalToHorizontalFieldOfView(fovHoriz, camera.aspect);
+            material.SetFloat(baseName + "FovHoriz", fovHoriz);
+            material.SetFloat(baseName + "FovVert", fovVert);
+
+            RenderTexture cameraTexture = camera.targetTexture;
+            cameraTexture.filterMode = FilterMode.Bilinear;
+
+            string name = "_TexCamera" + i.ToString();
+            material.SetTexture(name, cameraTexture);
+        }
+
+
+#if UNITY_EDITOR
+        // The following three functions are part of the complicated pattern necessary for
+        // a `leftProjectorIndex` value set in the Inspector to be acessible by `AdjoiningDisplaysCameraBuilder`
+        // at build time, when this value is used as an argument in the Windows shortcut file.
+        // TODO: Rename `AdjoiningDisplaysCameraBuilder` since it is now used here, too.
+
+        private void OnValidate()
+        {
+            leftProjectorIndexStatic = leftProjectorIndex;
+        }
+
+        private static int GetMonitorIndex()
+        {
+            return leftProjectorIndexStatic;
+        }
+
+        [InitializeOnLoadMethod]
+        public static void SetupDelegate()
+        {
+            // TODO: Rename `AdjoiningDisplaysCameraBuilder` since it is now used here, too.
+            AdjoiningDisplaysCameraBuilder.getMonitorIndexDelegate = GetMonitorIndex;
+        }
+#endif
+
+        private Material _material;
+
+        private Texture2D _projectorSurfaceXTexture;
+        private Texture2D _projectorSurfaceYTexture;
+        private Texture2D _projectorSurfaceZTexture;
+        private Texture2D _projectorSurfaceMaskTexture;
+        private Texture2D _projectorSurfaceColorCorrectionTexture;
+
+        private bool _enableSixCameras = false;
+    }
+}
