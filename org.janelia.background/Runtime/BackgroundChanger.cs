14b7c34a7afeb01e4a7748cb2c438fd720b19445
Philip M. Hubbard <hubbardp@janelia.hhmi.org>
2026-05-07 14:33:52 -0400

    Renames quitWhenDone to waitForTimeout.

--- org.janelia.background/Runtime/BackgroundChanger.cs
+++ org.janelia.background/Runtime/BackgroundChanger.cs
@@ -24,7 +24,8 @@ namespace Janelia
             public float durationSecs = 1;
             public string separatorTexture;
             public float separatorDurationSecs;
-            public bool quitWhenDone = true;
+            // True: quit per org.janelia.general timeoutSecs. False: quit when images are exhausted.
+            public bool waitForTimeout = false;
         }
 
         public static void Initialize(Spec spec, string specFilePath)
@@ -183,7 +184,7 @@ namespace Janelia
                 UseSeparatorTexture();
                 yield return new WaitForSeconds(_spec.separatorDurationSecs);
 
-                if (_spec.quitWhenDone)
+                if (!_spec.waitForTimeout)
                     Application.Quit();
             }
 
